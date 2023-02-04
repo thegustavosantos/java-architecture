359c0731be288a34d3bdb72f8976e1ca0877b698
Gusta <thegustavosantos@outlook.com>
2023-02-03 22:06:38 -0300

    solid

--- alura-rh-solid-example/src/br/com/alura/rh/model/Terceirizado.java
+++ alura-rh-solid-example/src/br/com/alura/rh/model/Terceirizado.java
@@ -1,13 +1,16 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
-public class Terceirizado extends Funcionario {
+public class Terceirizado {
 
+    private DadosPessoais dadosPessoais;
     private String empresa;
 
-    public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-        super(nome, cpf, cargo, salario);
+    public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, LocalDate dataUltimoReajuste, String empresa) {
+        this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+        this.empresa = empresa;
     }
 
     public String getEmpresa() {
